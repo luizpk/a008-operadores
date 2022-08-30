c357b2dc6e69992e6e8378c102e417cf9f901a55
LuizPK <luizpkpinheiro@gmail.com>
2022-08-30 14:40:16 -0300

    Resolvendo Exercicio2

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let numeroInformado = Number(prompt("Insira um número par."))
+console.log(numeroInformado%2)
+
+// Sempre está sendo impresso o número 0, tendo em vista que o 
+//resto de qualquer número inteiro par dividido por dois sempre será 0
+
+// QUando digitamos um número inteiro impar sempre será impresso o número 1, tendo em vista que o 
+//resto de qualquer número inteiro ímpar dividido por dois sempre será 1
