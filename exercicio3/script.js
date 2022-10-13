5abb9787bb815d0420c4f5c16e1d172405294865
LuizPK <luizpkpinheiro@gmail.com>
2022-10-13 15:46:35 -0300

    Revisão e atualização dos exercícios sobre operadores

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,14 +1,29 @@
+// # Exercício 3
 
-let operação1 = (5>20 && 5<2)
-console.log(operação1)
+// Escreva as operações lógicas abaixo em formato de código. Imprima no console se o valor da operação é verdadeira ou falsa:
 
-let operação2 = (5===5 || 5==="5")
-console.log(operação2)
+// a) 5 é maior que 20 e também é menor que 2;
 
-let operação3 = !20>50
-console.log(operação3)
+console.log("5 é maior que 20 e também é menor que 2", 5>=20&&5<=2)
 
-let operação4 = (!20>50 || 50>60)
-console.log(operação4)
+// b) 5 é igual a 5 ou é igual a “5”;
+
+console.log("5 é igual a 5 ou é igual a “5”", 5===5&&5==="5")
+
+// c) negação de (vinte é maior que cinquenta)
+
+console.log("negação de (vinte é maior que cinquenta)", !(20>50))
+
+// d) negação de (vinte é maior que cinquenta OU cinquenta é maior que sessenta)
+
+console.log("negação de (vinte é maior que cinquenta OU cinquenta é maior que sessenta)", !(20>50)||50>60)
 
 
+// Exemplo:
+
+// ```jsx
+// const operacao1 = 20 < 50 && 50 > 90;
+// console.log(operacao1);
+// /*false, porque 50 não é maior que 90, consequentemente temos true && false,
+// que devolve false*/
+// ```
